c97d54f93543a59e369f280faad90843ca6d788e
GRIKZHANA <grikzhanadaolaguput20@gmail.com>
2025-11-09 15:17:13 +0530

    Add files via upload

---
+++ Question23.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int n = 5;
+
+    for(int i = 1; i <= n; i++) {
+
+        // Leading spaces
+        for(int s = 1; s <= n - i; s++)
+            printf("  ");
+
+        // Stars
+        for(int j = 1; j <= n; j++)
+            printf("* ");
+
+        printf("\n");
+    }
+
+    return 0;
+}
